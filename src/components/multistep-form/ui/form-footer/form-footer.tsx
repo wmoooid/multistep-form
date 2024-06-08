39f8c6b877b37c00ee5261f2593697412a2ac330
wmoooid <threeqletters@yandex.ru>
2024-06-08 23:32:02 +0400

    form footer buttons

--- src/components/multistep-form/ui/form-footer/form-footer.tsx
+++ src/components/multistep-form/ui/form-footer/form-footer.tsx
@@ -1,5 +1,21 @@
+import { ArrowLeftIcon } from '@radix-ui/react-icons';
+
+import { Button } from '@/shared/ui/button';
+
 import styles from './form-footer.module.css';
 
 export const FormFooter = () => {
-    return <div className={styles.wrapper}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <Button type="button">
+        <ArrowLeftIcon />
+      </Button>
+
+      <Button
+        key="next"
+        type="button">
+        Следующий шаг
+      </Button>
+    </div>
+  );
 };
