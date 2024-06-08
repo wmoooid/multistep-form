98c837349efd797df522dd98a4c95c65abbf5b00
wmoooid <threeqletters@yandex.ru>
2024-06-09 01:29:36 +0400

    ccs modules config rework

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,34 @@
 import { resolve } from 'node:path';
 
 import react from '@vitejs/plugin-react-swc';
+import crypto from 'crypto';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src'),
-        },
-    },
-    server: {
-        port: 3000,
-    },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 3000
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: (name, filename, css) => {
+        const componentName = filename
+          .replace(/\.\w+$/, '')
+          .split('/')
+          .splice(-2)
+          .shift();
+
+        const hash = crypto.createHash('md5').update(css).digest('base64').substring(0, 5);
+
+        return `${componentName}__${name}_${hash}`;
+      }
+    }
+  }
 });
