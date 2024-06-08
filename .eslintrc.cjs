ce924d14ba841c61a283eabc1465d3438598d793
wmoooid <threeqletters@yandex.ru>
2024-06-08 23:29:38 +0400

    Eslint & prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,48 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:promise/recommended',
+    'plugin:import/warnings',
+    'plugin:import/typescript'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh', 'simple-import-sort', 'import'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
+    'import/no-default-export': 'warn',
+    'import/first': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/no-duplicates': 'warn',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/display-name': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
     ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn'
   },
-}
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
